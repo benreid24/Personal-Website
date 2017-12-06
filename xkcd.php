cc874e9f63717812ac607a15034405951439a538
Ben <reidben24@gmail.com>
2017-12-06 17:20:02 -0500

    Make xkcd bot page

--- xkcd.php
+++ xkcd.php
@@ -7,9 +7,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Ben Reid</title>
+	<title>Ben Reid - Peoplemon</title>
 
 	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/app.css">
 	<link rel="stylesheet" href="assets/css/sidebar-collapse.css">
 
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -27,9 +28,22 @@
 	</script>
 
 	<div class="main-content">
-		<div style="text-align: center"><img src="assets/images/logo.jpg" alt="arrow" height="340"></div>
-		<h1>Ben Reid</h1>
-		<p>I'm going to write a simple bio here I suppose. Something informative, interesting, but not dumb or too long. Test</p>
+		<h1 class="header">xkcd Stats Bot</h1>
+		<p style="content">
+			After seeing <a href="https://www.reddit.com/r/xkcd/comments/7g7isx/xkcd_transcriber_bot_statistics/" class="link">this</a> Reddit post, I decided to
+			try and develop my own Reddit bot. The purpose of this bot is to find references to xkcd comics on Reddit and compile data from those. From this data
+			I plan on computing various stats, such as which comics are the most popular, where they get posted the most, and who references them the most
+		</p>
+		<p class="content">
+			The bot currently identifies references based on links. It can parse xkcd links and determine which comic was referenced based on the comic number, or them
+			image name if that is what the link points to. The entire comment, parent comment (if any), corresponding ids, and the comic itself are all added to a sql
+			database. From this data the stats can be computed. Additionally, I was hoping to compute correlations in the parent comments, and other relatively simple
+			metrics, to see how feasible it is to predict a "relevant xkcd." I don't expect to be able to do this accurately, but it would be interesting to see how
+			far I can get without getting too crazy<p>
+		</p>
+		<p class="content">
+			The bot itself is implemented in Python as it is incredibly easy to interface to Reddit with. The source code and progress can be viewed on the
+			<a href="https://github.com/benreid24/xkcd_bot" class="link">GitHub</a> 
 	</div>
 
 </body>
